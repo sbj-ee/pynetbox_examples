d0d9da7a9bdc6747d8f19f5dc6c60c0076a82036
Stephen B. Johnson <49662809+sbj-ee@users.noreply.github.com>
2024-10-08 16:58:17 -0500

    Create test_contact.py

---
+++ test_contact.py
@@ -0,0 +1,58 @@
+from dotenv import dotenv_values
+import pynetbox
+import sys
+from pprint import pprint
+from time import sleep
+import urllib3
+urllib3.disable_warnings()
+
+
+def test_add_contact() -> None:
+    config = dotenv_values(".env.netbox.sbj.dev")
+
+    try:
+        token = config['token']
+        url = config['url']
+    except KeyError:
+        print("key missing from env file")
+        sys.exit()
+
+    nb = pynetbox.api(url=url, token=token)
+    nb.http_session.verify = False
+
+    contact_name: str = "Bob Dole"
+
+    rv = nb.tenancy.contacts.create(name=contact_name)
+
+    print(f"rv = {rv}")
+
+
+def test_delete_contact():
+    config = dotenv_values(".env.netbox.sbj.dev")
+
+    try:
+        token = config['token']
+        url = config['url']
+    except KeyError:
+        print("key missing from env file")
+        sys.exit()
+
+    nb = pynetbox.api(url=url, token=token)
+    nb.http_session.verify = False
+
+    contact_name: str = "Bob Dole"
+
+    contact_ref = nb.tenancy.contacts.get(name=contact_name)
+    print(f"contact_ref = {contact_ref}   type={type(contact_ref)}")
+    # print(dir(contact_ref))
+    print(f"contact id = {contact_ref.id}")
+    print(f"contact name = {contact_ref.name}")
+
+    rv = nb.tenancy.contacts.delete([contact_ref])
+    print(f"rv = {rv}")
+
+
+if __name__ == "__main__":
+    test_add_contact()
+    sleep(10)
+    test_delete_contact()
