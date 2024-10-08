97ba8801ce9dc12696ff711619efd37867d99d8a
Stephen B. Johnson <49662809+sbj-ee@users.noreply.github.com>
2024-10-08 17:18:17 -0500

    Update test_contact.py

--- test_contact.py
+++ test_contact.py
@@ -30,35 +30,6 @@ def test_add_contact() -> None:
         assert False
 
 
-def test_delete_contact():
-    config = dotenv_values(".env.netbox.sbj.dev")
-
-    try:
-        token = config['token']
-        url = config['url']
-    except KeyError:
-        print("key missing from env file")
-        sys.exit()
-
-    nb = pynetbox.api(url=url, token=token)
-    nb.http_session.verify = False
-
-    contact_name: str = "Bob Dole"
-
-    contact_ref = nb.tenancy.contacts.get(name=contact_name)
-    print(f"contact_ref = {contact_ref}   type={type(contact_ref)}")
-    # print(dir(contact_ref))
-    print(f"contact id = {contact_ref.id}")
-    print(f"contact name = {contact_ref.name}")
-
-    rv = nb.tenancy.contacts.delete([contact_ref])
-    
-    if rv:
-        assert True
-    else:
-        assert False
-
-
 def test_modify_contact():
     config = dotenv_values(".env.netbox.sbj.dev")
 
@@ -75,13 +46,32 @@ def test_modify_contact():
     contact_name: str = "Bob Dole"
 
     contact_ref = nb.tenancy.contacts.get(name=contact_name)
-    print(f"contact_ref = {contact_ref}   type={type(contact_ref)}")
-    # print(dir(contact_ref))
-    print(f"contact id = {contact_ref.id}")
-    print(f"contact name = {contact_ref.name}")
-    print("changing contact - title")
     contact_ref.title = "Miner yabba dabba do"
     contact_ref.save()
+
+
+def test_delete_contact():
+    config = dotenv_values(".env.netbox.sbj.dev")
+
+    try:
+        token = config['token']
+        url = config['url']
+    except KeyError:
+        print("key missing from env file")
+        sys.exit()
+
+    nb = pynetbox.api(url=url, token=token)
+    nb.http_session.verify = False
+
+    contact_name: str = "Bob Dole"
+
+    contact_ref = nb.tenancy.contacts.get(name=contact_name)
+    rv = nb.tenancy.contacts.delete([contact_ref])
+    
+    if rv:
+        assert True
+    else:
+        assert False
 
 
 if __name__ == "__main__":
