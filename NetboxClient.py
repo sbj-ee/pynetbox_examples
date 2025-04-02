0574aa2d1fc5f0d66724fcc406d0076d854be869
Stephen B. Johnson <49662809+sbj-ee@users.noreply.github.com>
2025-04-02 02:01:05 -0500

    Update NetboxClient.py

--- NetboxClient.py
+++ NetboxClient.py
@@ -1,6 +1,10 @@
 import pynetbox
 from loguru import logger
 from getpass import getpass
+import urllib3
+from ipaddress import ip_address, IPv4Address
+
+urllib3.disable_warnings()
 
 
 class NetboxClient:
@@ -29,6 +33,33 @@ class NetboxClient:
         """get the netbox version"""
         return str(self.nb.status()["netbox-version"])
 
+    def check_if_cidr_exists(self, cidr: str) -> bool:
+        """given a CIDR, check to see if that CIDR is in netbox"""
+        result = False
+        try:
+            result = self.nb.ipam.ip_addresses.get(address=cidr)
+        except pynetbox.RequestError as e:
+            print(e.error)
+
+        if result:
+            return True
+        else:
+            return False
+
+    def get_cidr_from_ip(self, ip: str) -> str:
+        """"given an IP, return the CIDR if it exists in netbox"""
+        max_bitrange: int = 128
+        ip_type = ip_address(ip)
+        if isinstance(ip_type, IPv4Address):
+            max_bitrange: int = 32
+
+        for slash in range(max_bitrange, 0, -1):
+            cidr = f"{ip}/{slash}"
+            result = self.nb.ipam.ip_addresses.get(address=cidr)
+
+            if result:
+                return cidr
+
 
 if __name__ == "__main__":
     netbox_url = input("Netbox URL: ")
@@ -36,3 +67,6 @@ if __name__ == "__main__":
     nb_client = NetboxClient(netbox_url, netbox_token)
     nb_client.setup_logging()
     print(nb_client.get_pynetbox_version())
+    ip_addr = "216.170.130.221"
+    print(f"check if IP exists in netbox: {nb_client.check_if_cidr_exists(ip_addr)}")
+    print(nb_client.get_cidr_from_ip(ip_addr))
