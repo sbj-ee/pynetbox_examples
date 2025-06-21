88062647c4fe873e0b7f831e1c7c4e993a50932e
Stephen B. Johnson <49662809+sbj-ee@users.noreply.github.com>
2025-06-20 21:29:04 -0500

    added interface types dict

---
+++ netbox_interface_types.py
@@ -0,0 +1,98 @@
+interface_types = {
+    # Virtual interfaces
+    "Virtual": "virtual",
+    "LAG": "lag",
+
+    # Ethernet (Copper)
+    "10BASE-T": "10base-t",
+    "100BASE-TX": "100base-tx",
+    "1000BASE-T": "1000base-t",
+    "2.5GBASE-T": "2.5gbase-t",
+    "5GBASE-T": "5gbase-t",
+    "10GBASE-T": "10gbase-t",
+    "25GBASE-T": "25gbase-t",
+
+    # Ethernet (Fiber)
+    "100BASE-FX": "100base-fx",
+    "1000BASE-X": "1000base-x",
+    "10GBASE-X": "10gbase-x",
+    "25GBASE-X": "25gbase-x",
+    "40GBASE-X": "40gbase-x",
+    "50GBASE-X": "50gbase-x",
+    "100GBASE-X": "100gbase-x",
+    "200GBASE-X": "200gbase-x",
+    "400GBASE-X": "400gbase-x",
+
+    # Ethernet (Backplane)
+    "1000BASE-KX": "1000base-kx",
+    "10GBASE-KR": "10gbase-kr",
+    "10GBASE-KX4": "10gbase-kx4",
+    "25GBASE-KR": "25gbase-kr",
+    "40GBASE-KR4": "40gbase-kr4",
+    "50GBASE-KR": "50gbase-kr",
+    "100GBASE-KR": "100gbase-kr",
+
+    # Wireless
+    "IEEE 802.11a": "ieee802.11a",
+    "IEEE 802.11b/g": "ieee802.11b-g",
+    "IEEE 802.11n": "ieee802.11n",
+    "IEEE 802.11ac": "ieee802.11ac",
+    "IEEE 802.11ax": "ieee802.11ax",
+
+    # Cellular
+    "2G": "2g",
+    "3G": "3g",
+    "4G": "4g",
+    "5G": "5g",
+
+    # FiberChannel
+    "Fibre Channel - 1G": "fibrechannel-1g",
+    "Fibre Channel - 2G": "fibrechannel-2g",
+    "Fibre Channel - 4G": "fibrechannel-4g",
+    "Fibre Channel - 8G": "fibrechannel-8g",
+    "Fibre Channel - 16G": "fibrechannel-16g",
+    "Fibre Channel - 32G": "fibrechannel-32g",
+    "Fibre Channel - 64G": "fibrechannel-64g",
+    "Fibre Channel - 128G": "fibrechannel-128g",
+
+    # InfiniBand
+    "InfiniBand SDR": "infiniband-sdr",
+    "InfiniBand DDR": "infiniband-ddr",
+    "InfiniBand QDR": "infiniband-qdr",
+    "InfiniBand FDR10": "infiniband-fdr10",
+    "InfiniBand FDR": "infiniband-fdr",
+    "InfiniBand EDR": "infiniband-edr",
+    "InfiniBand HDR": "infiniband-hdr",
+    "InfiniBand NDR": "infiniband-ndr",
+
+    # Serial
+    "T1": "t1",
+    "E1": "e1",
+    "T3": "t3",
+    "E3": "e3",
+
+    # ATM
+    "ATM (DS3)": "atm-ds3",
+    "ATM (OC3)": "atm-oc3",
+    "ATM (OC12)": "atm-oc12",
+    "ATM (OC48)": "atm-oc48",
+
+    # SONET
+    "SONET (OC3)": "sonet-oc3",
+    "SONET (OC12)": "sonet-oc12",
+    "SONET (OC48)": "sonet-oc48",
+    "SONET (OC192)": "sonet-oc192",
+    "SONET (OC768)": "sonet-oc768",
+
+    # DSL
+    "ADSL": "adsl",
+    "SDSL": "sdsl",
+    "VDSL": "vdsl",
+
+    # GPON
+    "GPON": "gpon",
+
+    # Other
+    "Other": "other"
+}
+
