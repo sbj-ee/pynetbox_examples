cd155fa513a677c0d1e33709c732d467028a1620
Stephen B. Johnson <49662809+sbj-ee@users.noreply.github.com>
2025-12-03 13:28:33 -0600

    Create get_maintenance_value.py

---
+++ get_maintenance_value.py
@@ -0,0 +1,27 @@
+import os
+import pynetbox
+import urllib3
+
+urllib3.disable_warnings()
+
+NETBOX_URL = os.getenv("NETBOX_URL")
+NETBOX_TOKEN = os.getenv("NETBOX_TOKEN")
+
+nb = pynetbox.api(NETBOX_URL, token=NETBOX_TOKEN)
+nb.http_session.verify = False
+
+def get_maintenance(obj):
+    return obj.custom_fields.get("Maintenance", False) if obj.custom_fields else False
+
+# Examples:
+devices = nb.dcim.devices.all()
+for dev in devices:
+    print(f"Device {dev.name}: Maintenance = {get_maintenance(dev)}")
+
+circuits = nb.circuits.circuits.all()
+for circuit in circuits:
+    print(f"Circuit {circuit.cid}: Maintenance = {get_maintenance(circuit)}")
+
+interfaces = nb.dcim.interfaces.all()
+for iface in interfaces:
+    print(f"Interface {iface.device.name}/{iface.name}: Maintenance = {get_maintenance(iface)}")
